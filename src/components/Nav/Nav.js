b806fcb4d136f9e9332d7e066839cdce488b4937
Choiboi500 <jbc8894@gmail.com>
2020-08-16 12:20:32 -0400

    nav completed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Route, Link } from "react-router-dom";
+import About from "../About/About";
+import Home from "../Home/Home";
+
 
 class Nav extends Component {
     render() {
         return (
             <div>
+                <nav>
+                    <h1>FlickCritic</h1>
+                    <Link to="/"> Home </Link>
+                    <Route exact path="/"
+                        component={Home}
+                    />
+
+                    <Link to="/about"> About </Link>
+                    <Route exact path="/about"
+                        component={About} />
+                </nav>
+
             </div>
         );
     }
