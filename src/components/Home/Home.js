0b0733640998022b3750c2ac8a49a12d1b690e70
roshoniab <roshoniabrooks@gmail.com>
2020-08-16 17:08:08 -0500

    made fetch call in home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-// import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 class Home extends Component {
-
+	constructor() {
+		super();
+		this.state = {
+			posters: [],
+		};
+	}
+	componentDidMount() {
+		const url = `https://flick-critic-db.herokuapp.com/api/movies`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log({ poster: res });
+			});
+	}
 
 	render() {
 		return (
 			<div>
-				{/* <main>{this.state.posters.map((poster) =>{
-                    return <
-                })}</main> */}
+				<main>
+					{this.state.posters.map((poster) => {
+						return (
+							<Link to={'/info/'}>
+								<img src=' ' />
+							</Link>
+						);
+					})}
+				</main>
 			</div>
 		);
 	}
