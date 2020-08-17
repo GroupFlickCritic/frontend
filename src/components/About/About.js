048a7a873cb6b2ac9f4ae713b9dbcfe14478fb94
Choiboi500 <jbc8894@gmail.com>
2020-08-17 16:01:08 -0400

    imported Jumbotron for About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap';
+import { Accordion, Card, Button, Jumbotron } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function About() {
 	function handleOnClick(e) {
@@ -8,7 +9,7 @@ function About() {
 	}
 
 	return (
-		<div className='About'>
+		<Jumbotron className='About'>
 			<h1>MEET OUR TEAM</h1>
 			<p>
 				We all are very different. We come from different backgrounds. We were
@@ -17,25 +18,29 @@ function About() {
 				is flickcritic. The countless hours spent was all worth it to make our
 				dreams a reality. Thank you from all of us for visiting our app. We hope
 				you enjoyed it as much as we enjoyed making flickcritic.
-			</p>
+  </p>
+			
+		
 
-			<Accordion defaultActiveKey='1'>
-				<Card>
-					<Card.Header>
-						<Accordion.Toggle
-							onClick={handleOnClick}
-							className='panel-toggle'
-							variant='link'
-							eventKey='0'>
-							CONTACT US
+
+
+		<Accordion defaultActiveKey='1'>
+			<Card>
+				<Card.Header>
+					<Accordion.Toggle
+						onClick={handleOnClick}
+						className='panel-toggle'
+						variant='link'
+						eventKey='0'>
+						CONTACT US
 						</Accordion.Toggle>
-					</Card.Header>
-					<Accordion.Collapse eventKey='0'>
-						<Card.Body>our contact info</Card.Body>
-					</Accordion.Collapse>
-				</Card>
-			</Accordion>
-		</div>
+				</Card.Header>
+				<Accordion.Collapse eventKey='0'>
+					<Card.Body>our contact info</Card.Body>
+				</Accordion.Collapse>
+			</Card>
+		</Accordion>
+		</Jumbotron>
 	);
 }
 
