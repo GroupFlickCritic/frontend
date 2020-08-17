adb768e0492908f829d18d2fa1e4ba30261310a8
roshoniab <roshoniabrooks@gmail.com>
2020-08-17 13:11:55 -0500

    added text to the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+		
 
+// function myFunction() {
+// 			function
+// 		}
 class About extends Component {
 	render() {
+
 		return (
 			<div>
 				<header>
-					<h1>This is the About page</h1>
+					<h1>MEET OUR TEAM</h1>
+					<p>
+						We all are very different. We come from different backgrounds. We
+						were born in different cities, at a different time. We have
+						different hobbies, and interest. Although wee may be different what
+						connects us all is flickcritic. The countless hours spent was all
+						worth it to make our dreams a reality. Thank you from all of us for
+						visiting our app. We hope you enjoyed it as much as we enjoyed
+						making flickcritic
+					</p>
+					{/* <button onclick={handleClick}>CONTACT US</button> */}
 				</header>
 			</div>
 		);
