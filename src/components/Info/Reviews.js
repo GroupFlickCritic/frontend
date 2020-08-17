e3675441581f05d1d308d68a712136baa66124d8
Choiboi500 <jbc8894@gmail.com>
2020-08-17 13:34:22 -0400

    Reviews init commit, constructor and handle change created

--- src/components/Info/Reviews.js
+++ src/components/Info/Reviews.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Form from './Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+class Reviews extends React.Component {
+    //constructor for user input from Form
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchString: '',
+            lastSearch: '',
+            galleryImages: '',
+            setSearch: false,
+            error: false,
+        };
+    }
+    //find value in Form
+    handleChange = (event) => {
+        this.setState({ searchString: event.target.value });
+    };
 
-class Reviews extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            </ListGroup>
         );
     }
 }
