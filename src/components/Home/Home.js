4766cf54bec90968b67f80e94125a22549e38b9d
Choiboi500 <jbc8894@gmail.com>
2020-08-18 16:36:24 -0400

    deleted img file, added hover zoom to home imgs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,22 +3,26 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card'
 import CardColumns from 'react-bootstrap/CardColumns';
 import Container from 'react-bootstrap/Container';
+import MDBView from 'react-bootstrap/MDBView'
 class Home extends Component {
-	
+
 	render() {
 		let movies = this.props.movies.map((movie, index) => {
 			return (
 				//we want the keys to be unique
-				<Link to={`/info/${movie.title}`} key={index + movie.title}>
-					<Card>
-						<Card.Img
-							variant='top'
-							src={movie.mainImage}
-							alt={movie.title}
-							className='info'
-						/>
-					</Card>
-				</Link>
+				<MDBView hover zoom>
+					<Link to={`/info/${movie.title}`} key={index + movie.title}>
+						<Card>
+							<Card.Img
+								variant='top'
+								src={movie.mainImage}
+								alt={movie.title}
+								className='info'
+							/>
+						</Card>
+					</Link>
+				</MDBView>
+
 			);
 		});
 		return (
