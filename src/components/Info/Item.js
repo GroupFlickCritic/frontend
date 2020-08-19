56f5a7e6a22c632bad27a744bd28059447915b2d
alishalawani <alishalawani@gmail.com>
2020-08-19 17:38:48 -0500

    creatd the edit feature

---
+++ src/components/Info/Item.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { ListGroup, Button, Row, Col } from 'react-bootstrap';
+import Moment from 'moment';
+import { Form as Input } from 'react-bootstrap';
+import axios from 'axios'
+let url = 'https://flick-critic-db.herokuapp.com/api/reviews/';
+class Item extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			editClicked: true,
+			updatedReview: {
+				review: this.props.review,
+				datePosted: '',
+			},
+		};
+	}
+	handleEdit = (event) => {
+		event.preventDefault();
+		this.setState({ editClicked: false });
+	};
+	handleChange = (event) =>{
+        event.preventDefault()
+		this.setState({
+			updatedReview: {
+				review: `${event.target.value}`,
+				datePosted: new Date(),
+			},
+        });
+
+        //  this.props.review = this.state.updatedReview.review
+    }
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		//pushing a new object of the new review and datePosted
+		axios.put(url+this.props.id, this.state.updatedReview )
+			.then((res) => {
+				window.location.reload();
+            })
+            
+	};
+
+	editForm = () => {
+		return (
+			<Input onSubmit={this.handleSubmit}>
+				<Row>
+					<Col>
+						<Input.Group>
+							<Input.Control
+								size='sm'
+								placeholder='Write your review here'
+								type='text'
+								name='searchString'
+								required
+								onChange={this.handleChange}
+								value={this.state.updatedReview.review}
+								rows='1'
+							/>
+						</Input.Group>
+					</Col>
+					<Col>
+						<Button type='submit' variant='dark' className='btn-sm'>
+							Submit
+						</Button>
+					</Col>
+				</Row>
+			</Input>
+		);
+	};
+	render() {
+		return this.state.editClicked ? (
+			<ListGroup.Item id='reviews'>
+				"{this.props.review}" <br />
+				{Moment(this.props.datePosted).add(1, 'days').format('L')}{' '}
+				<span className='edit' onClick={this.handleEdit}>
+					edit
+				</span>
+			</ListGroup.Item>
+		) : (
+			this.editForm()
+		);
+	}
+}
+
+export default Item;
--- src/components/Info/Reviews.js
+++ src/components/Info/Reviews.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { ListGroup, Container } from 'react-bootstrap';
-import Moment from 'moment';
+
+import { Row, Col, Button } from 'react-bootstrap';
+import Item from './Item';
 import './Reviews.css';
 const url1 = 'https://flick-critic-db.herokuapp.com/api/movies/';
 const url2 = 'https://flick-critic-db.herokuapp.com/api/reviews/';
-let newer;
 class Reviews extends React.Component {
 	//constructor for user input from Form
 	constructor(props) {
 		super(props);
 		this.state = {
-            movie: '',
-            brandNew: '',
-			reviews: '',
+			movie: '',
 			reviewList: [],
-			setSearch: false,
+			editClicked: true,
 			error: false,
 		};
 	}
@@ -28,20 +27,19 @@ class Reviews extends React.Component {
 			})
 			.then(() => {
 				this.state.movie.reviews.forEach((rev, index) => {
-
-                    
 					fetch(url2 + rev)
 						.then((res) => {
 							return res.json();
 						})
 						.then((rev) => {
-                            this.setState({reviewList: [...this.state.reviewList, rev],})
-                            console.log(rev);
-                        newer = <li>{rev.review}</li>
+							this.setState({ reviewList: [...this.state.reviewList, rev] });
+							console.log(rev);
 						});
-                });
+				});
 			});
 	}
+
+	
 
 	//find value in Form
 	handleChange = (event) => {
@@ -54,15 +52,27 @@ class Reviews extends React.Component {
 		this.getData(this.state.searchString);
 	};
 
+	
 	render() {
-        let newRevs = this.state.reviewList.map((review, index) => {
-					console.log(review);
-                    return  <ListGroup.Item id='reviews' key={index}>
-                                "{review.review}" <br />
-                                {Moment(review.datePosted).add(1, 'days').format('L')}
-                            </ListGroup.Item>;
-                });
-                console.log(newRevs)
+		let newRevs = this.state.reviewList.map((review, index) => {
+			console.log(review);
+
+			return <Item review={review.review} datePosted={review.datePosted} id={review._id}/>
+
+			// return this.state.editClicked ? (
+			// 	<ListGroup.Item id='reviews' key={index}>
+			// 		"{review.review}" <br />
+			// 		{Moment(review.datePosted).add(1, 'days').format('L')}{' '}
+			// 		<span className='edit' onClick={this.handleEdit} key={index}>
+			// 			edit
+			// 		</span>
+			// 	</ListGroup.Item>
+			// ) : (
+			// 	this.editForm()
+			// );
+			// return this.editForm();
+		});
+		console.log(newRevs);
 		return (
 			<Container id='reviews'>
 				<ListGroup id='reviews' variant='flush'>
